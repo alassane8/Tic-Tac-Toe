35f475edd831118cb8b1214476aec9377534ce1b
alassane8 <alassane.wade@edu.ece.fr>
2024-01-01 18:57:27 +0100

    feat: add constructor

--- code/tictactoe.java
+++ code/tictactoe.java
@@ -1,87 +1,32 @@
-import java.util.Random;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Tictactoe {
-    public int number;
-    public char[][] Array;
+public class Tictactoe implements ActionListener {
+    private JFrame frame;
+    private JPanel panel;
+    private JButton[] buttons = new JButton[9];
 
-    public Tictactoe(int num, char[][] array) {
-        number = num;
-        Array = array;
-    }
+    public Tictactoe() {
+        frame = new JFrame("TicTacToe game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 3));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-    public String display(String display) {
-
-        for (char row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-
-                if (row == 0 && col == 2) {
-                    display = display + Array[row][col] + "\n";
-                } else if (row == 1 && col == 2) {
-                    display = display + Array[row][col] + "\n";
-                } else {
-                    display = display + Array[row][col] + " ";
-                }
-            }
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            buttons[i].setFont(new Font("Arial", Font.PLAIN, 40));
+            buttons[i].addActionListener(this);
+            panel.add(buttons[i]);
         }
-        return display;
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Variables
-        int num = 0;
-        int input = 1;
-        int value;
-        char c = 'X';
-        String str;
-        String display = "";
-        char[][] array = { { 49, 50, 51 }, { 52, 53, 54 }, { 55, 56, 57 } };
-
-        // Welcoming the player
-        JOptionPane.showMessageDialog(null, "Welcome to Tic-Tac-Toe.\n"
-                + "You will have X and the computer will have O.");
-
-        // Display of the array that will be the grid for players
-        Tictactoe tableau = new Tictactoe(num, array);
-
-        for (int a = 0; a < 3; a++) {
-            for (int b = 0; b < 3; b++) {
-                do {
-                    // Input of the shoot the player hase to make
-                    str = JOptionPane.showInputDialog(null,
-                            "Enter number where you want to play.\n" + tableau.display(display));
-                    num = Integer.parseInt(str);
-                    num = num + 48;
-                    // Going accross the grid to find num
-                    for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 3; j++) {
-                            if (array[i][j] == num) {
-                                array[i][j] = c;
-                                JOptionPane.showMessageDialog(null,
-                                        "Your move is recorded.\nNow the computer will play.\n"
-                                                + tableau.display(display));
-                            }
-                        }
-                    }
-                    // Computer shoot
-                    Random random = new Random();
-                    value = random.nextInt(10) + 1;
-                    value = value + 48;
-                    // Going accross the grid to found num
-                    for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 3; j++) {
-                            while (array[i][j] == value && array[i][j] == c) {
-                                value = random.nextInt(10) + 1;
-                            }
-                            if (array[i][j] == value) {
-                                array[i][j] = 'O';
-                                JOptionPane.showMessageDialog(null,
-                                        "The computer has played.\n" + tableau.display(display));
-                            }
-                        }
-                    }
-                } while (input != 0);
-            }
-        }
+        new Tictactoe();
     }
 }
